08ceb3ba98564e7d41390dc461daea8bc9334d67
SilemNabib <silemnabib@gmail.com>
2024-03-07 09:21:59 -0500

    build: create the django project

---
+++ ccsaproject/ccsaproject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ccsaproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccsaproject.settings')
+
+application = get_wsgi_application()
