5425fdb954af3134ca4f5b00ed41ebcb4f518f25
SilemNabib <silemnabib@gmail.com>
2024-03-08 11:21:49 -0500

    build: rename the django project

--- ccsaProject/asgi.py
+++ ccsaProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ccsaproject project.
+ASGI config for ccsaProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccsaproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccsaProject.settings')
 
 application = get_asgi_application()
--- ccsaProject/urls.py
+++ ccsaProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ccsaproject project.
+URL configuration for ccsaProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
