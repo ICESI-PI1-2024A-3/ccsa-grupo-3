ca1fc23d36309b9f07f02a1b41e861952b4ac1b5
juanC773 <juan_cg_g@hotmail.com>
2024-03-10 18:40:12 -0500

    filter and superuser function

--- postgraduateManagement/admin.py
+++ postgraduateManagement/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Materia, Curso, Clase
+from .models import Materia, Curso, Clase, Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'telephone', 'user_group')
+    search_fields = ('user__username', 'user__groups__name')
+    list_filter = ('user__groups',)
+
+    def user_group(self, obj):
+        return ", ".join([group.name for group in obj.user.groups.all().order_by('name')])
+
+    user_group.short_description = 'Grupo'
 
 admin.site.register(Materia)
 admin.site.register(Curso)
 admin.site.register(Clase)
-# Register your models here.
+admin.site.register(Profile, ProfileAdmin)
--- postgraduateManagement/models.py
+++ postgraduateManagement/models.py
@@ -1,6 +1,37 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario')
+    address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Direccion')
+    telephone = models.CharField(max_length=50, null=True, blank=True, verbose_name='Telefono')
+    identification = models.CharField(max_length=30, null=True, blank=True, verbose_name='Identificacion')
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.user.username
+
+    
+  
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+        
+def save_user_profile(sender, instance, **kwargs):
+    instance.usuario.save()
+
+    
+post_save.connect(create_user_profile, sender= User)
+post_save.connect(save_user_profile, sender= User)
+    
+    
 
 # Create your models here.
 class Departamento(models.Model):
