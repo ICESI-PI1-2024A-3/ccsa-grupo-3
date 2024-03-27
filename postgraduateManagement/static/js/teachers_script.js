07ce1f39f58f1dc43efb205d6314829c9321fa59
SilemNabib <silemnabib@gmail.com>
2024-03-27 09:26:54 -0500

    feat(HU-03 | 03): show teachers
    show the photo, name, lastname, email and city of all teachers in the database

--- postgraduateManagement/static/js/teachers_script.js
+++ postgraduateManagement/static/js/teachers_script.js
@@ -1,5 +1,25 @@
+const triggers = document.querySelectorAll('.trigger');
+    triggers.forEach(trigger => {
+        trigger.addEventListener('click', function(event) {
+            event.preventDefault();
+            const card = this.closest('.card');
+            card.classList.toggle('show');
+        });
+});
+
+/*
 const trigger = document.querySelector('.trigger'),
 addclass = document.querySelector('.card');
-trigger.addEventListener('click', function() {
+trigger.addEventListener('click', function(event) {
+    event.preventDefault();
     addclass.classList.toggle('show');
-})
+});
+*/
+
+function copy_teacher_email(cedula) {
+    var copyText = document.getElementById(cedula);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the text: " + copyText.value);
+}
--- postgraduateManagement/views/views_teacher.py
+++ postgraduateManagement/views/views_teacher.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from postgraduateManagement.models import Docente
+
 
 # Create your views here.
 def show_all(request):
-    return render(request, 'teachers.html')
+    teacher_list = Docente.objects.all()
+    return render(request, 'teachers.html', {"teacher_list": teacher_list})
