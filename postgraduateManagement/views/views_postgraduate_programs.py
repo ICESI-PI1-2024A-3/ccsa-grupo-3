b629e4c786fedb801207cc305856cd822bcd373a
alejoamu <alejoamu2@gmail.com>
2024-03-30 17:19:21 -0500

    build: change the structure of the view and its methods

--- postgraduateManagement/views/views_postgraduate_programs.py
+++ postgraduateManagement/views/views_postgraduate_programs.py
@@ -4,15 +4,15 @@ from django.views import View
 
 
 class ProgramsView(View):
-    @staticmethod
-    def get(request):
+    def get(self, request):
         programasListados = Programa.objects.all()
+        return render(request, "programas_posgrado.html", {"programas": programasListados})
+
+    def post(self, request):
+        programa_codigo = request.POST.get('programa_codigo')
         programa_seleccionado = None
-
-        if request.method == 'POST':
-            programa_codigo = request.POST.get('programa_codigo')
-            if programa_codigo:
-                programa_seleccionado = Programa.objects.get(
-                    codigo=programa_codigo)
-
+        if programa_codigo:
+            programa_seleccionado = Programa.objects.get(
+                codigo=programa_codigo)
+        programasListados = Programa.objects.all()
         return render(request, "programas_posgrado.html", {"programas": programasListados, "programa_seleccionado": programa_seleccionado})
