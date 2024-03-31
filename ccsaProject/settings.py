e3385cb560edfba538c696ff54a74e0988cac742
SilemNabib <silemnabib@gmail.com>
2024-03-31 17:17:22 -0500

    fix: solve settings.py wrong middleware

--- ccsaProject/settings.py
+++ ccsaProject/settings.py
@@ -53,8 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.LoginRequiredMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'ccsaProject.urls'
