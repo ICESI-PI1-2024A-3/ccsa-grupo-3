0b975597c38aad01d19dc8dc6ec4ecdc6e6f0ab0
Johan Daniel Aguirre Arias <aguirreariasjohandaniel@gmail.com>
2024-05-05 20:36:20 -0500

    test: upload the test for change teacher state

--- postgraduateManagement/test/test_selenium_sprint_2.py
+++ postgraduateManagement/test/test_selenium_sprint_2.py
@@ -41,3 +41,24 @@ class LiveServerTestCase(LiveServerTestCase):
         assert "Contratos" in driver.title
         driver.back()
 
+    def test_change_state_teacher(self):
+        driver = self.scrap()
+        username = driver.find_element(By.NAME, "username")
+        password = driver.find_element(By.NAME, "password")
+        submit = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
+        username.send_keys('mrzen')
+        password.send_keys('1026')
+        submit.click()
+        driver.execute_script("document.getElementById('docentes').click();")
+        driver.execute_script("document.getElementById('changestate57-596-5887').click();")
+        estado = driver.find_element(By.NAME, "estado")
+        estado.clear()
+        estado.send_keys("Inactivo")
+        button = driver.find_element(By.CLASS_NAME, "btn-submit")
+        assert "Docentes" in driver.title
+        status = driver.find_element(By.NAME, "status")
+        status.send_keys("inactivo")
+        buttonsubmit = driver.find_element(By.ID, "submit")
+        buttonsubmit.click()
+        docente = driver.find_element(By.ID, "teacher57-596-5887")
+        assert docente.text.__contains__("Mano")
