55dc5c9960f61f2716b97e9ddd23f89c41c7ca3b
gerson05 <1115449382@u.icesi.edu.co>
2024-05-12 12:32:13 -0500

    test: update test

--- postgraduateManagement/test/test_contracts.py
+++ postgraduateManagement/test/test_contracts.py
@@ -3,12 +3,15 @@ from multiprocessing.connection import Client
 
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 from postgraduateManagement.models import Contrato, Docente,TipoContrato, Ciudad, EstadoContrato
 
 class ViewContractTestCase(TestCase):
     def setUp(self):
         date_str = "12/03/21"
         fecha_elaboracion = datetime.strptime(date_str, "%d/%m/%y").strftime("%Y-%m-%d")
+        self.user = User.objects.create_user(username='testuser', password='12345')
 
         self.ciudad = Ciudad.objects.create(id=1, nombre='San Francisco')
         self.estadoContrato = EstadoContrato.objects.create(id=1, nombre='activo')
@@ -19,12 +22,38 @@ class ViewContractTestCase(TestCase):
     def test_view_contract(self):
 
         response = self.client.get(reverse('ver_contratos'))
-
-
         self.assertEqual(response.status_code, 302)
+
     def test_view_contract2(self):
         response = self.client.get(reverse('ver_contratos'))
         response = self.client.get(response.url)
+
+    def test_edit_contract3(self):
+        response = self.client.get(reverse('editar_contratos', kwargs={'codigo': self.contrato.codigo}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_contract4(self):
+        response = self.client.get(reverse('editar_contratos', kwargs={'codigo': self.contrato.codigo}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_editing_contract_redirect(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('edicion_contratos'), {
+            'txtCodigo': self.contrato.codigo,
+            'fecha_elaboracion': '2024-05-12',
+            'tipo_contrato': self.tipoContrato.codigo,
+            'estado_contrato': self.estadoContrato.id,
+            'docente': self.docente.cedula
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/viewContract')
+
+    def test_edit_contract6(self):
+        response = self.client.get(reverse('editar_contratos', kwargs={'codigo': "gfh"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/accounts/login/', response.url)
+
 
 
 
